ee8538fce6e9de03d893389e83c12ed5d51e2d97
hosseiny998@gmail.com <hosseiny998@gmail.com>
2022-11-12 03:54:26 +0330

    fibonatichi

---
+++ sum/fibonatchi2.py
@@ -0,0 +1,16 @@
+fibs = {0: 0, 1: 1}
+
+
+def fib(n):
+    if n in fibs:
+        return fibs[n]
+
+    i = 2
+    while i <= n:
+        fibs[i] = fibs[i - 2] + fibs[i - 1]
+        i += 1
+
+    return fibs[n]
+
+
+print(fib(11))
