5d7b7fef16355bac2c6a16705786f1a5ed775eac
hosseiny998@gmail.com <hosseiny998@gmail.com>
2022-11-14 02:01:15 +0330

    Merge sort

---
+++ sort/merge_sort.py
@@ -0,0 +1,58 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+
+        # Finding the mid of the array
+        mid = len(arr) // 2
+
+        # Dividing the array elements
+        L = arr[:mid]
+
+        # into 2 halves
+        R = arr[mid:]
+
+        # Sorting the first half
+        merge_sort(L)
+
+        # Sorting the second half
+        merge_sort(R)
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+# Code to print the list
+
+def print_list(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+
+# Driver Code
+if __name__ == '__main__':
+    arr = [12, 11, 23, 54, 54, 55, 52, 13, 5, 6, 7]
+    print("Given array is", end="\n")
+    print_list(arr)
+    merge_sort(arr)
+    print("Sorted array is: ", end="\n")
+    print_list(arr)
